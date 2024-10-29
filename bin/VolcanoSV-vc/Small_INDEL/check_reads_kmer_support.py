1bf11a1502ba7efaed6b1054c436fa2b73ab9a5c
LUO CAN <luoc1@maizie2.vampire>
2024-10-29 12:25:06 -0500

    fix bug

--- bin/VolcanoSV-vc/Small_INDEL/check_reads_kmer_support.py
+++ bin/VolcanoSV-vc/Small_INDEL/check_reads_kmer_support.py
@@ -202,7 +202,7 @@ def filter_indel( prefix,vcffile, indel_kmer_file, reads_bamfile, output_folder,
 
     # add debugger
     for i in range(len(var_list)):
-        if len(len(sequences[i])) == 0:
+        if len(sequences[i]) == 0:
             print("encounter empty kmer support array!")
             print("Var information: ", f"{i}-th var ",var_list[i])
             exit()
